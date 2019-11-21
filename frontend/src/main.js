f5583ee814197c8238a6f3d215ba46c53c6fb957
Armando Jaleo <armando@armandojaleo.com>
2019-11-21 03:11:51 +0100

    Delete material

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,6 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-
-Vue.use(VueMaterial)
 
 import './plugins/bootstrap-vue'
 import App from './App.vue'
